819102b1d1852f38056579d4ec9a7adb7bc260e1
ophir <ophir_g1@hotmail.com>
2018-04-18 09:12:34 +0300

    first commit

---
+++ app/src/main/java/com/apps/brz/cardstack/MainActivity.java
@@ -0,0 +1,15 @@
+package com.apps.brz.cardstack;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+        setContentView(R.layout.activity_main);
+    }
+
+}
